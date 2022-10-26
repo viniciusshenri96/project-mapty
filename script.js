ee31b70a77ffbdaf4ea8b0cb9c1a717234ee747b
Vinicius <vinicius_henrique96@hotmail.com>
2022-10-26 12:03:47 -0300

    :sparkles: feat: Implementing a Geolocation API

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// API do navegador para saber a localização atual do usuario, com dois Callback, uma de sucesso se conseguir a localização e a outra erro se caso não consiga a localização.
+
+// Using the Geolocation API
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(
+        `https://www.google.com.br/maps/@${latitude},${longitude},15z`
+      );
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
+
+// Displaying a Map Using Leaflet Library
